ef92715427a3f848790f901146e095e0f9847c76
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-07-30 08:51:06 +0000

    [Finish] bookers2-good＆comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,13 +2,17 @@ class BookCommentsController < ApplicationController
   
  
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     if comment.save
-    redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-    redirect_to book_path(book)
+      #コメントのバリデーションの設定
+      @book_comment = comment
+      @user = @book.user
+      @newbook = Book.new
+      render "books/show" 
     end
   end
   
